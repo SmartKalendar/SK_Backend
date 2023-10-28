ab7c6438f9b80edfb4f14babbad25163950ad096
cpprhtn <cpprhtn@naver.com>
2023-10-29 00:35:32 +0900

    test: csr

---
+++ csrtest.py
@@ -0,0 +1,26 @@
+import sys
+import requests
+import os
+from dotenv import load_dotenv
+
+# .env 파일 로드
+load_dotenv()
+
+client_id = os.getenv("CSR_ID")
+client_secret = os.getenv("CSR_SECRET")
+audio = os.getenv("AUDIO_FILE")
+
+lang = "Kor" # 언어 코드 ( Kor, Jpn, Eng, Chn )
+url = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=" + lang
+data = open('ttt.mp3', 'rb')
+headers = {
+    "X-NCP-APIGW-API-KEY-ID": client_id,
+    "X-NCP-APIGW-API-KEY": client_secret,
+    "Content-Type": "application/octet-stream"
+}
+response = requests.post(url,  data=data, headers=headers)
+rescode = response.status_code
+if(rescode == 200):
+    print (response.text)
+else:
+    print("Error : " + response.text)
